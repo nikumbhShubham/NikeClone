ae5c18dd84515e9adeb8af39050d4d35608127ad
nikumbhShubham <shubhamnikumbh19@gmail.com>
2024-03-06 23:16:12 +0530

    Nike web Frontend

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+
+
+export {
+    Hero,
+    PopularProduct,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    SuperQuality,
+    Footer
+}
